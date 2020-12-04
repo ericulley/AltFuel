a1178be790d17a38c3decc79d2ca1a35b4c9cb75
Your Name <you@example.com>
2020-12-03 18:59:38 -0700

    added basic styling for the page and inputs

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
+// AJAX Alternative Fuel Search Function
+const altFuelSearch = (lat, long, type, radius) => {
+    $.ajax({
+        url: `https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=nobIa8uA9o0Vu32ocLEdOCpQcv0DB5nYBrvYNB0F&fuel_type=E85,ELEC,BD&latitude=${lat}&longitude=${long}&fuel_type=ELEC&limit=10&access_code=public`,
+    }).then(
+        (data) => {
+            console.log(data)
+        },
+        (error) => {
+            console.log(`There was an AJAX error: ${error}.`)
+        }
+    )
+}
+
+
 $(() => {
-    // Define Global Variables
-    let curLat = null
-    let curLong = null
+// Start by getting user location coordinates or zip code
+navigator.geolocation.getCurrentPosition((position) => {
+    // If access is granted to current location, code below will run
+    let curLat = position.coords.latitude
+    let curLong = position.coords.longitude
+    altFuelSearch(curLat, curLong)
+    },
+    (error) => {
+    // If locatoin data access is denied, code below will run & ask for zip code input
+        console.log("error: user didn't give current location.")
 
-    // Get user location coordinates
-    const getUserLoc = () => {
-        navigator.geolocation.getCurrentPosition((position) => {
-          curLat = position.coords.latitude
-          curLong = position.coords.longitude
-          console.log(curLat, curLong)
-        });
     }
-
-    // AJAX
-    const altFuelSearch = (curLat, curLong) => {
-        $.ajax({
-            url: `https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=E85,ELEC,HY,BD&state=CA&limit=5&api_key=nobIa8uA9o0Vu32ocLEdOCpQcv0DB5nYBrvYNB0F`,
-        }).then(
-            (data) => {
-                console.log(data)
-            },
-            (error) => {
-                console.log(`There was an AJAX error: ${error}.`)
-            }
-        )
-    }
+);
 
 
 
-getUserLoc()
 
 
+// Milage Range Slider Styling
+$('#range-slider').on('input', () => {
+    $('#slider-val').text($('#range-slider').val())
+})
 
 
 })
